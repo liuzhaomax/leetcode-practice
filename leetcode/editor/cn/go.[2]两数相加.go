ad1ca290e770793b536c8f50306c4dfcfe4e595a
LIU Zhao <liuzhaomax@163.com>
2022-01-26 19:17:11 +1100

    25

--- leetcode/editor/cn/go.[2]两数相加.go
+++ leetcode/editor/cn/go.[2]两数相加.go
@@ -56,10 +56,10 @@ import (
 //leetcode submit region begin(Prohibit modification and deletion)
 
 //Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	ret := new(ListNode)

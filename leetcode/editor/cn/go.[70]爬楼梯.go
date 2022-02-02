5d70cca731d901fd755c9f01dbb03ba9327b8735
LIU Zhao <liuzhaomax@163.com>
2022-02-02 23:00:42 +1100

    70

--- leetcode/editor/cn/go.[70]爬楼梯.go
+++ leetcode/editor/cn/go.[70]爬楼梯.go
@@ -45,7 +45,23 @@ package main
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func climbStairs(n int) int {
-
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n-1]
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+//func main() {
+//	n := 45
+//	fmt.Println(climbStairs(n))
+//}
